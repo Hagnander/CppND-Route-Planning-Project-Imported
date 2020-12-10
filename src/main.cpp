0c44fbb84cd6a7c2343dcd84ec09a0ecbe6a2704
Hagnander <johan.hagnander@movinity.ai>
2020-12-10 15:13:23 +0100

    TODO 1 completed

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,8 @@
 #include "route_planner.h"
 
 using namespace std::experimental;
+using std::cin;
+using std::cout;
 
 static std::optional<std::vector<std::byte>> ReadFile(const std::string &path)
 {   
@@ -55,12 +57,28 @@ int main(int argc, const char **argv)
     // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
     // user input for these values using std::cin. Pass the user input to the
     // RoutePlanner object below in place of 10, 10, 90, 90.
+    float temp = 101; //out of range
+    std::vector<float> coordinates(4);
+    std::vector<std::string> message={"Write x start value", "Write y start value", "Write x goal value", "Write y goal value"};
+    int i = 0;
+    
+    while (i<4)
+    {
+        cout<<message[i]<<", the value must be between 0 and 100"<<"\n";
+        cin>>temp;
+        if ((0.0<= temp) && (temp<= 100.0))
+        {
+            coordinates[i]=temp;
+            temp=101; //out of range
+            i++;
+        }
+    }
 
     // Build Model.
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, coordinates[0], coordinates[1], coordinates[2], coordinates[3]};
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
